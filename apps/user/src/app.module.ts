568ba4496ce8eaca477edf42dd7b8fb3924d446c
Taehwan Kim <obtusa07@gmail.com>
2025-03-13 20:23:39 +0900

    feat: set up Joi validation and ConfigModule

--- apps/user/src/app.module.ts
+++ apps/user/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 @Module({
     imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            validationSchema: Joi.object({
+                HTTP_PORT: Joi.number().required(),
+                DB_URL: Joi.string().required(),
+            })
+        }),
         UserModule,
         AuthModule,
     ],
