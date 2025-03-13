f9726d6347c46dcae0a1fa13fec506e9b44a079b
Taehwan Kim <obtusa07@gmail.com>
2025-03-13 19:12:40 +0900

    feat: set up default auth

--- apps/user/src/auth/auth.controller.ts
+++ apps/user/src/auth/auth.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register-dto';
 
 @Controller()
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
+
+  @Post('register')
+  registerUser(token: string, @Body() registerDto: RegisterDto) {
+    if (token === null) {
+      throw new UnauthorizedException('トークンを入力してください');
+    }
+    // return this.authService.register(token, registerDto);
+  }
 }
