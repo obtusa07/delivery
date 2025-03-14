ce9aa345cd8493dde37e758491bc5139be7188ae
Taehwan Kim <obtusa07@gmail.com>
2025-03-15 01:11:17 +0900

    feat: add endpoint to create sample data in DB

--- apps/product/src/product/product.module.ts
+++ apps/product/src/product/product.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './entity/product.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forFeature([Product])
+  ],
   controllers: [ProductController],
   providers: [ProductService],
 })
-export class ProductModule {}
+export class ProductModule { }
--- apps/product/src/product/product.service.ts
+++ apps/product/src/product/product.service.ts
@@ -1,8 +1,55 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './entity/product.entity';
 
 @Injectable()
 export class ProductService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>
+  ) { }
+
+  async createSampels() {
+    const data = [
+      {
+        name: 'プリン',
+        price: 300,
+        description: '美味しいジャージープリン',
+        stock: 3,
+      },
+      {
+        name: 'ラーメン',
+        price: 150,
+        description: '美味しくて辛いラーメン',
+        stock: 1,
+      },
+      {
+        name: 'ケーキ',
+        price: 500,
+        description: 'ふわふわで甘いショートケーキ',
+        stock: 2,
+      },
+      {
+        name: 'おにぎり',
+        price: 200,
+        description: '具材たっぷりの美味しいおにぎり',
+        stock: 5,
+      },
+      {
+        name: 'カレー',
+        price: 700,
+        description: 'スパイシーでコクのあるカレー',
+        stock: 4,
+      },
+      {
+        name: '寿司',
+        price: 1200,
+        description: '新鮮なネタを使った美味しい寿司',
+        stock: 3,
+      }
+    ]
+    await this.productRepository.save(data);
+    return true;
   }
 }
