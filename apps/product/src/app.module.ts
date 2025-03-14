38123581fc40277ef4192945813c4c7ab5e4f90a
Taehwan Kim <obtusa07@gmail.com>
2025-03-15 00:56:13 +0900

    feat: add postgres_product compose.yaml and some configs

--- apps/product/src/app.module.ts
+++ apps/product/src/app.module.ts
@@ -1,8 +1,26 @@
 import { Module } from "@nestjs/common";
 import { ProductModule } from "./product/product.module";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import * as Joi from 'joi';
 
 @Module({
     imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            validationSchema: Joi.object({
+                DB_URL: Joi.string().required(),
+            })
+        }),
+        TypeOrmModule.forRootAsync({
+            useFactory: (configService: ConfigService) => ({
+                type: 'postgres',
+                url: configService.getOrThrow('DB_URL'),
+                autoLoadEntities: true,
+                synchronize: true,
+            }),
+            inject: [ConfigService]
+        }),
         ProductModule
     ],
 })
