531084a3bb136a6aac4c48855cb0681eda0e7a30
Taehwan Kim <obtusa07@gmail.com>
2025-03-16 14:25:00 +0900

    feat: add order entities

---
+++ apps/order/src/order/entity/payment.entity.ts
@@ -0,0 +1,30 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+export enum PaymentMethod {
+    creditCard = 'CreditCard',
+    applePay = 'ApplePay'
+}
+
+@Schema({
+    _id: false,
+})
+export class Payment {
+    @Prop()
+    paymentId: string;
+    @Prop({
+        enum: PaymentMethod,
+        default: PaymentMethod.creditCard,
+    })
+    paymentMethod: PaymentMethod;
+
+    @Prop({
+        required: true
+    })
+    paymentName: string;
+    @Prop({
+        required: true
+    })
+    amount: number;
+}
+
+export const PaymentSchema = SchemaFactory.createForClass(Payment);
