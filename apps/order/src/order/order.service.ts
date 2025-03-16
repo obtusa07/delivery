130dfa6aa1059aa8df693597a7223250d4f25161
Taehwan Kim <obtusa07@gmail.com>
2025-03-16 20:39:10 +0900

    feat: create a blueprint of order app

--- apps/order/src/order/order.service.ts
+++ apps/order/src/order/order.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { CreateOrderDto } from './dto/create-order.dto';
 
 @Injectable()
 export class OrderService {
-  getHello(): string {
-    return 'Hello World!';
+  async createOrder(createOrderDto: CreateOrderDto, token: string) {
+    // get user information
+
+    // get product infomation
+    // calculate total price
+
+    // verify price
+
+    // create Order
+
+    // try to pay
+
+    // update order status
+
+    // return result
   }
 }
