295bc9f0a7abfff603c1ed686b9f9fcc4bbfed5d
Taehwan Kim <obtusa07@gmail.com>
2025-03-16 15:04:53 +0900

    feat: add Payment entity

---
+++ apps/payment/src/payment/entity/payment.entity.ts
@@ -0,0 +1,53 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+export enum PaymentStatus {
+    pending = 'Pending',
+    rejected = 'Rejected',
+    approved = 'Approved',
+}
+
+export enum PaymentMethod {
+    creditCard = 'CreditCard',
+    applePay = 'ApplePay',
+}
+
+export enum NotificationStatus {
+    pending = 'Pending',
+    sent = 'Sent',
+}
+
+@Entity()
+export class Payment {
+    @PrimaryGeneratedColumn('uuid')
+    id: string
+
+    @Column({
+        enum: PaymentStatus,
+        default: PaymentStatus.pending
+    })
+    paymentStatus: PaymentStatus;
+
+    @Column({
+        enum: PaymentMethod,
+        default: PaymentMethod.creditCard,
+    })
+    paymentMethod: PaymentMethod;
+
+    @Column()
+    cardNumber: string;
+    @Column()
+    expiryYear: string;
+    @Column()
+    expiryMonth: string;
+    @Column()
+    birthOrRegistration: string;
+
+    @Column()
+    passwordTwoDigits: string;
+
+    @Column({
+        enum: NotificationStatus,
+        default: NotificationStatus.pending,
+    })
+    notificationStatus: NotificationStatus
+}
